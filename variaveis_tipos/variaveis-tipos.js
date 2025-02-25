eb148e9b5d43f0b5b1511c8f84e5dc0506a814d6
Lucas de Souza Guerra <114541871+Baycad@users.noreply.github.com>
2025-02-25 10:47:32 -0300

    Add files via upload
    Atividade 1 feita em sala no dia 25/02/2025

---
+++ variaveis_tipos/variaveis-tipos.js
@@ -0,0 +1,36 @@
+const n = 3;
+//n = 5;
+
+var j = 2;
+var j = 3;
+
+//console.log('j='+j);
+
+let i = 4;
+i = 6;
+
+//console.log('i='+i);
+
+let nome = "João";
+cpf = '123';
+telefone = '123';
+
+let tString = `Meu nome é ${nome}. Meu CPF é ${cpf}. Meu telefone é ${telefone}`;
+
+//console.log(tString);
+
+let x = 5.0;
+x = .5;
+x = 3;
+
+//console.log(typeof x);
+//console.log(typeof tString);
+
+let verdadeiro = true;
+let falso = false;
+
+let conexão = null;
+let conexão1;
+
+let y = x * x;
+console.log('y='+y);
